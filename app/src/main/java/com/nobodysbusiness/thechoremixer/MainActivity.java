589fa9db492052805d5d7497b453d6be8f3bd054
Philip Morse <philipmorse1234@gmail.com>
2019-04-21 22:30:06 -0400

    Added tabs

--- app/src/main/java/com/nobodysbusiness/thechoremixer/MainActivity.java
+++ app/src/main/java/com/nobodysbusiness/thechoremixer/MainActivity.java
@@ -1,17 +1,31 @@
 package com.nobodysbusiness.thechoremixer;
 
 import android.database.Cursor;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager mViewPager = findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        UserMainTabAdapter adapter = new UserMainTabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new GenerateChoreFragment(), "New Chore");
+        viewPager.setAdapter(adapter);
 
     }
 }
